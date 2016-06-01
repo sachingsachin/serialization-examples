206e3b70dbbc99cebe62e3b7c8a24221edbbcb52
Sachin Goyal <sgoyal6@L-SB8M1HEG8W-M.local>
2016-06-01 13:44:18 -0700

    Add a failing test for circular refs

--- src/test/java/examples_group/serialization/CircularRefsTest.java
+++ src/test/java/examples_group/serialization/CircularRefsTest.java
@@ -1,31 +1,57 @@
 package examples_group.serialization;
 
-import examples_group.serialization.data.House;
-import examples_group.serialization.data.PojoBuilder;
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import static org.junit.Assert.assertTrue;
+
+import java.io.ByteArrayOutputStream;
+
+import org.apache.avro.Schema;
+import org.apache.avro.io.DatumWriter;
+import org.apache.avro.io.Encoder;
+import org.apache.avro.io.EncoderFactory;
+import org.apache.avro.reflect.ReflectData;
+import org.apache.avro.reflect.ReflectDatumWriter;
+import org.junit.Test;
 
 /**
  * Check circular references
  */
-public class CircularRefsTest extends TestCase
+public class CircularRefsTest
 {
-    public void testSerialization()
+    @Test
+    public void testSerialization() throws Exception
     {
-        House house = PojoBuilder.buildHouse(true);
-        assertTrue( true );
-    }
+        // Create a circular linked-list
+        ListNode first = new ListNode("first");
+        ListNode second = new ListNode("second");
+        second.next = first;
+        first.next = second;
 
-    /**
-     * Creates the test case
-     */
-    public CircularRefsTest(String testName)
-    {
-        super(testName);
+        ReflectData rdata = ReflectData.AllowNull.get();
+
+        // Print Schema
+        Schema schema = rdata.getSchema(ListNode.class);
+        System.out.println(schema);
+
+        // Serialize
+        DatumWriter<ListNode> datumWriter = new ReflectDatumWriter<ListNode>(ListNode.class);
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        Encoder encoder = EncoderFactory.get().binaryEncoder(byteArrayOutputStream, null);
+        datumWriter.write(first, encoder);
+        encoder.flush();
+        byte[] bytes = byteArrayOutputStream.toByteArray();
+
+        assertTrue( bytes != null );
     }
-    public static Test suite()
-    {
-        return new TestSuite(CircularRefsTest.class);
+}
+
+class ListNode
+{
+    String data;
+    ListNode next;
+
+    public ListNode() {
+    }
+    public ListNode(String data) {
+        this.data = data;
     }
 }
